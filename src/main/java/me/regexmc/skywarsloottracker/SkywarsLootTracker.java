0027216cf47a86b4a241e4078c5ddb5ed6407748
Regex <56909681+RegexMC@users.noreply.github.com>
2021-03-15 20:57:19 +1100

    Base forge mod project

---
+++ src/main/java/me/regexmc/skywarsloottracker/SkywarsLootTracker.java
@@ -0,0 +1,17 @@
+package me.regexmc.skywarsloottracker;
+
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+
+@Mod(modid = SkywarsLootTracker.MODID, version = SkywarsLootTracker.VERSION)
+public class SkywarsLootTracker
+{
+    public static final String MODID = "skywarsloottracker";
+    public static final String VERSION = "1.0";
+
+    @Mod.EventHandler
+    public void init(FMLInitializationEvent event)
+    {
+
+    }
+}
